35dff5b46c7dccdd5f5e28ee2938ee798bde2ce0
Nikita Slyvkanych <nikita.slyvkanych@corp.kismia.com>
2021-02-09 22:43:05 +0200

    module1-task3: add console tools (generate, help, version)

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.MAX_DIGIT = 9;
+
+module.exports.DEFAULT_COMMAND = `--help`;
+
+module.exports.USER_ARGV_INDEX = 2;
+
+module.exports.ExitCode = {
+  error: 1,
+  success: 0,
+};
+
---
+++ src/service/cli/generate.js
@@ -0,0 +1,43 @@
+const fs = require(`fs`);
+const {getRandomInt, shuffle, getPictureFileName} = require(`../../utils`);
+const {
+  DEFAULT_COUNT,
+  FILE_NAME,
+  TITLES,
+  SENTENCES,
+  CATEGORIES,
+  OfferType,
+  SumRestrict,
+  PictureRestrict,
+} = require(`../../data`);
+
+'use strict';
+
+const generateOffers = (count) => (
+  Array(count).fill({}).map(() => ({
+    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+    description: shuffle(SENTENCES).slice(1, 5).join(` `),
+    picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
+    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
+    sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
+  }))
+);
+
+
+module.exports = {
+  name: `--generate`,
+  run(args) {
+    const [count] = args;
+    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    const content = JSON.stringify(generateOffers(countOffer));
+
+    fs.writeFile(FILE_NAME, content, (err) => {
+      if (err) {
+        return console.error(`Can't write data to file...`);
+      }
+
+      return console.info(`Operation success. File created.`);
+    });
+  }
+};
