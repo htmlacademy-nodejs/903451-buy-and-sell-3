557afd939d9c5ffdfae4ed3857d562655a23f94c
Nikita Slyvkanych <nikita.slyvkanych@corp.kismia.com>
2021-03-12 13:10:46 +0200

    module3-task2: add pug templates for pages and add rendering to routes

--- src/express/routes/offer.router.js
+++ src/express/routes/offer.router.js
@@ -4,10 +4,9 @@ const {Router} = require(`express`);
 
 const offerRouter = new Router();
 
-offerRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id ${req.params.id}`));
-offerRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id ${req.params.id}`));
-offerRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
-offerRouter.get(`/id`, (req, res) => res.send(`/offers/id`));
-offerRouter.get(`/`, (req, res) => res.send(`/offers`));
+offerRouter.get(`/category/:id`, (req, res) => res.render(`category`));
+offerRouter.get(`/edit/:id`, (req, res) => res.render(`offers/ticket-edit`));
+offerRouter.get(`/add`, (req, res) => res.render(`offers/new-ticket`));
+offerRouter.get(`/:id`, (req, res) => res.render(`/offers/ticket`));
 
 module.exports = offerRouter;
